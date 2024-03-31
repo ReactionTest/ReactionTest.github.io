34626f59f68f43f498eb6ad3b8ed45fd8e89451c
Priteshmewada <pritzz2013@gmail.com>
2024-03-31 20:05:35 +0530

    asd

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@
 
     // PRE LOADER
     $(window).load(function(){
-      $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
+      $('.preloader').delay(50).slideUp('slow'); // set duration in brackets    
     });
 
     // NAVBAR
